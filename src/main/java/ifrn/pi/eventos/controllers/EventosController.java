fa721c18011f494762cf0e8da6f0617896756ebf
Joana <joana.isabelle@escolar.ifrn.edu.br>
2026-09-01 01:02:58 -0300

    Enviando dados do formulário para aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -2,12 +2,28 @@ package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
-	
+
 	@RequestMapping("/eventos/form")
-   public String form () {
-	   return "formEvento";
-   }
+	public String form() {
+		return "formEvento";
+	}
+
+	@RequestMapping("/eventos")
+
+	public String salvar(Evento evento) {
+
+		System.out.println("Nome: " + evento.getNome());
+
+		System.out.println("Local: " + evento.getLocal());
+
+		System.out.println("Data: " + evento.getData());
+
+		System.out.println("Horário: " + evento.getHorario());
+
+		return "eventoSalvo";
+	}
 }
